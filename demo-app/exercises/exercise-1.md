89bab577760f4d243599726ac34f1a5e53454c7a
Eric Greene <eric@t4d.io>
2022-02-21 15:55:10 -0500

    add exercise 1

---
+++ demo-app/exercises/exercise-1.md
@@ -0,0 +1,15 @@
+# Exercise 1
+
+## Requirements
+
+1. Create a new module named Car Tool
+
+Example: ng g m color-tool
+
+2. Create a new component named Car Home
+
+Example: ng g c color-tool/components/color-home
+
+3. Display the Car Home component beneath the Color Home component within App Component
+
+4. Ensure it works!
--- demo-app/src/app/app.module.ts
+++ demo-app/src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
+
+// ES2015 Module Import - JavaScript feature
 import { BrowserModule } from '@angular/platform-browser';
+
+import { ColorToolModule  } from './color-tool/color-tool.module';
 
 import { AppComponent } from './app.component';
 
+
+// Angular Module
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ColorToolModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
